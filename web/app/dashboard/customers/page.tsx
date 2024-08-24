d7181961a658aa73fc7e78270839f97dd0d78ebc
Priyanshu Parikh <parikhpriyanshu7@gmail.com>
2024-08-24 23:48:31 +0530

    added sleekplan's feedback management integration

--- web/app/dashboard/customers/page.tsx
+++ web/app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+export default function CustomersPage() {
+  const productId = process.env.NEXT_PUBLIC_SLEEKPLAN_PRODUCT_ID;
+  return (
+    <div className="h-full">
+      <iframe className="h-full" src={`https://embed-${productId}.sleekplan.app/`}></iframe>
+    </div>
+  )
 }
--- web/components/ui/dashboard/nav-links.tsx
+++ web/components/ui/dashboard/nav-links.tsx
@@ -13,11 +13,6 @@ import clsx from "clsx";
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: "Home", href: "/dashboard", icon: HomeIcon },
-  {
-    name: "Invoices",
-    href: "/dashboard/invoices",
-    icon: DocumentDuplicateIcon,
-  },
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
