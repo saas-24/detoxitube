81b8e1ef99cc3bb4cc7a45431dbeca874572c697
Priyanshu Parikh <parikhpriyanshu7@gmail.com>
2024-08-26 03:54:59 +0530

    improved ui of dashboard and feedback management

--- web/app/dashboard/customers/page.tsx
+++ web/app/dashboard/customers/page.tsx
@@ -1,8 +1,8 @@
 export default function CustomersPage() {
   const productId = process.env.NEXT_PUBLIC_SLEEKPLAN_PRODUCT_ID;
   return (
-    <div className="h-full">
-      <iframe className="h-full" src={`https://embed-${productId}.sleekplan.app/`}></iframe>
+    <div className="h-full flex justify-center">
+      <iframe className="h-full w-2/3" src={`https://embed-${productId}.sleekplan.app/`}></iframe>
     </div>
   )
 }
--- web/app/dashboard/layout.tsx
+++ web/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from '@/components/ui/dashboard/sidenav';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="w-full flex-none md:w-64 bg-card px-2">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
